161486f4533aedfd9f5a1c48dc122611b130b4be
AhmetBSerce <a.bilgehan.serce@gmail.com>
2023-05-04 11:02:50 +0300

    Changes on "servo.py"

--- img_classification.py
+++ img_classification.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import tensorflow as tf
 # from smbus2 import SMBus as smbus
+from time import sleep
 
 # Model dosyasının yolu
 MODEL_PATH = os.getcwd() + "\model.tflite"
@@ -10,9 +11,6 @@ MODEL_PATH = os.getcwd() + "\model.tflite"
 # Sınıf etiketleri (one-hot encoding)
 LABELS = ['No Bottle', 'Plastic', 'Paper', 'Glass', 'Metal']
 ONE_HOT = [hex(int(''.join(map(str, i)), 2)) for i in np.eye(len(LABELS)).astype(np.int8)]
-
-# I2C ile gönderilecek veri
-bus = smbus.SMBus(1)
 
 # Girdi görüntüsünün boyutları
 INPUT_SIZE = (224, 224)
@@ -29,15 +27,17 @@ output_details = interpreter.get_output_details()
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 while True:
     # Kameradan bir görüntü alma
     ret, frame = cam.read()
-
+    
+    # Girdi Görüntüsünü fotoğraf olarak kaydetme ve atama
+    cv2.imwrite('frame.jpg', frame)
+    img = cv2.imread('frame.jpg')
+    
     # Girdi görüntüsünü model için uygun boyuta getirme ve normalizasyon
-    img = cv2.resize(frame, INPUT_SIZE)
-    img = img / 255.0
-    img = np.expand_dims(img, axis = 0)
-    img = img.astype(np.float32)
+    img = np.expand_dims(cv2.resize(frame, INPUT_SIZE) / 255.0, axis=0).astype(np.float32)
 
     # Girdi tensorunu modelde çalıştırma
     interpreter.set_tensor(input_details[0]['index'], img)
@@ -47,16 +47,21 @@ while True:
     output = interpreter.get_tensor(output_details[0]['index'])
     label_idx = np.argmax(output)
     text = LABELS[label_idx] + f" ({output[0][label_idx] * 100:.2f}%)"
-
+    
+    # Sınıf etiketini one-hot encoding'e çevirme
+    label_one_hot = ONE_HOT[label_idx]
+    
     # Görüntüye sınıf etiketini ekleme
     cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     # Görüntüyü gösterme
-    cv2.imshow('Classification', frame)
+    cv2.imshow('Classification', img)
 
     # Çıkış tuşuna basıldığında programı sonlandırma
     if cv2.waitKey(1) == ord('q'):
         break
+    
+    sleep(1)
 
 # Temizleme
 cam.release()
