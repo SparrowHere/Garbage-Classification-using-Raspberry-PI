eaedf84b4592354e46961d6f2160343e569ab86c
AhmetBSerce <a.bilgehan.serce@gmail.com>
2023-05-04 14:23:00 +0300

    - Updated "img_classification.py" - Added "serial_comm.ino" - Serial communication added

--- img_classification.py
+++ img_classification.py
@@ -4,13 +4,13 @@ import numpy as np
 import tensorflow as tf
 # from smbus2 import SMBus as smbus
 from time import sleep
+import serial
 
 # Model dosyasının yolu
 MODEL_PATH = os.getcwd() + "\model.tflite"
 
 # Sınıf etiketleri (one-hot encoding)
 LABELS = ['No Bottle', 'Plastic', 'Paper', 'Glass', 'Metal']
-ONE_HOT = [hex(int(''.join(map(str, i)), 2)) for i in np.eye(len(LABELS)).astype(np.int8)]
 
 # Girdi görüntüsünün boyutları
 INPUT_SIZE = (224, 224)
@@ -25,13 +25,13 @@ output_details = interpreter.get_output_details()
 
 # Kamera ayarları
 cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while True:
     # Kameradan bir görüntü alma
     ret, frame = cam.read()
-
+    
     # Girdi Görüntüsünü fotoğraf olarak kaydetme ve atama
     cv2.imwrite('frame.jpg', frame)
     img = cv2.imread('frame.jpg')
@@ -46,17 +46,36 @@ while True:
     # Çıktı tensorunu alma ve sınıf etiketini bulma
     output = interpreter.get_tensor(output_details[0]['index'])
     label_idx = np.argmax(output)
-    text = LABELS[label_idx] + f" ({output[0][label_idx] * 100:.2f}%)"
-    
-    # Sınıf etiketini one-hot encoding'e çevirme
-    label_one_hot = ONE_HOT[label_idx]
+    text = LABELS[label_idx] + f" ({output[0][label_idx] * 100:.2f}"
     
     # Görüntüye sınıf etiketini ekleme
     cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     # Görüntüyü gösterme
     cv2.imshow('Classification', frame)
-
+    
+    try:
+        # Arduino ile seri haberleşme başlatma
+        s = serial.Serial(
+            "COM9",
+            9600,
+            parity = serial.PARITY_NONE,
+            stopbits = serial.STOPBITS_ONE
+        )
+        
+        sleep(2)
+        
+        s.reset_input_buffer()
+        print("Serial communication has started.")
+        
+        # Sınıf etiketini Arduino'ya gönderme
+        s.write(f"{LABELS[label_idx]}\n".encode())
+    except KeyboardInterrupt:
+        print("Serial communication has ended.")
+        
+        # Herhangi bir klavye tuşuna basıldığında seri haberleşmeyi sonlandırma
+        s.close()
+    
     # Çıkış tuşuna basıldığında programı sonlandırma
     if cv2.waitKey(1) == ord('q'):
         break
